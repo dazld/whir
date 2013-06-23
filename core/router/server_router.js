dabecf13ff0eb8a8672873105ec4277f4147823f
Dan Peddle <dan.peddle@gmail.com>
2013-06-24 00:44:40 +0200

    cleanup

--- core/router/server_router.js
+++ core/router/server_router.js
@@ -2,8 +2,27 @@ var Backbone = require('backbone');
 var bus = require('../bus');
 
 var ServerRouter = Backbone.Router.extend({
-	startRouting: function(){
+	bus: bus,
+	routes: {
+		"":"index",
+		"release/:id":"release",
+		"release":"release"
 
+	},
+	index: function(){
+		this.bus.publish('routed',{
+			page: "default"
+		});
+	},
+	release: function(id){
+
+		id = id || false;
+		
+		this.bus.publish('routed',{
+			id: id,
+			page: "release"
+		});
+		
 	}
 });
 
--- index.js
+++ index.js
@@ -3,19 +3,16 @@ var Q = require('q');
 var bus = require('./core/bus');
 var hb = require('handlebars');
 
-// var postal = Postal();
 var PageFactory = require('./core/PageFactory')
     app = express();
 
 var factory = new PageFactory();
 
-
-
 bus.subscribe('request.in', function(data) {
     data.res.send();
 });
 
-app.get('/', function(req, res, next) {
+app.get('*', function(req, res, next) {
     bus.publish('request.in', {
         req: req,
         res: res,
